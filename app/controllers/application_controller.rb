62b3cc406d83ad19f25546b1e5d0483363811223
Murage-Ivy <ivymurage2000@gmail.com>
2023-04-14 12:57:51 +0300

    Install dependencies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::API
+  def encode_token(poyload)
+    @token = JWT.encode(payload, ENV["my_s3cr3t"])
+  end
+
+  def auth_header
+    request.headers["Authorization"]
+  end
+
+  def decode_token
+  end
+
+  def current_user
+  end
+
+  def logged_in?
+  end
 end
