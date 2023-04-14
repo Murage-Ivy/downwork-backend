6ebc3fc8e7392549320e174071b85917512178ab
Murage-Ivy <ivymurage2000@gmail.com>
2023-04-14 13:28:53 +0300

    Add validation for user model and relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+  has_secure_password
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  validates :username, uniqueness: { case_sensitive: false }
+  validates :username, presence: true
+  validates :password, presence: true
+  validates :password, length: { minimum: 6 }
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :email, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :image_url, presence: true
+  validates :image_url, format: { with: URI::regexp(%w(http https)) }
 end
