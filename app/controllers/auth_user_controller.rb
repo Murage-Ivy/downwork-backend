5c6290bfe42e435e095552e1d41d728d3c5e87f0
Murage-Ivy <ivymurage2000@gmail.com>
2023-04-14 15:14:22 +0300

    Create auth action controllers

---
+++ app/controllers/auth_user_controller.rb
@@ -0,0 +1,26 @@
+class AuthUserController < ApplicationController
+  skip_before_action :authorized, only: [:create]
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+  def profile
+    render json: { user: UserSerializer.new(current_user) }, status: :accepted
+  end
+
+  def create
+    @user = User.find_by(user_params[:username])
+    if @user&.authenticate(user_params[:password])
+      @token = encode_token(user_id: @user.id)
+      cookies["token"] = @token
+      byebug
+      render json: @user, serializer: UserSerializer, status: :created
+    else
+      render json: { error: "Invalid username or password" }, status: :unauthorized
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :password)
+  end
+end
