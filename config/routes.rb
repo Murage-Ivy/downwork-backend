f8bc383747e42ec5c06d9dfdbb591254a6022987
Murage-Ivy <ivymurage2000@gmail.com>
2023-04-14 14:19:48 +0300

    Migrate

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,4 @@ Rails.application.routes.draw do
 
   post "/signup", to: "users#create"
   # post "/login", to: "users#login"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
