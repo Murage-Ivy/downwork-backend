3691114c819b0f1164875b295c60d5956eb1ec9f
Murage-Ivy <ivymurage2000@gmail.com>
2023-04-14 13:36:44 +0300

    Add validation for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    belongs_to :user
+    has_many :comments, dependent: :destroy
+    validates :title, presence: true
+    validates :title, length: { minimum: 3 }
+    validates :description, presence: true
+    validates :description, length: { minimum: 10 }
+    validates :image_url, presence: true
+    validates :image_url, format: { with: URI::regexp(%w(http https)) }
+    validates :likes, presence: true
+    validates :likes, numericality: { only_integer: true }
+    validates :likes, numericality: { greater_than_or_equal_to: 0 }
+    validates :user_id, presence: true
+    validates :user_id, numericality: { only_integer: true }
+    validates :user_id, numericality: { greater_than: 0 }
 end
