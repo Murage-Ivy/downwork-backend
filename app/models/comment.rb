1db1804ea622e9b8289b780db2c67c85ab8f8b50
Murage-Ivy <ivymurage2000@gmail.com>
2023-04-14 13:39:00 +0300

    Add validation for comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,12 @@
 class Comment < ApplicationRecord
+    belongs_to :user
+    belongs_to :post
+    validates :content, presence: true
+    validates :content, length: { minimum: 3 }
+    validates :user_id, presence: true
+    validates :user_id, numericality: { only_integer: true }
+    validates :user_id, numericality: { greater_than: 0 }
+    validates :post_id, presence: true
+    validates :post_id, numericality: { only_integer: true }
+    validates :post_id, numericality: { greater_than: 0 }
 end
