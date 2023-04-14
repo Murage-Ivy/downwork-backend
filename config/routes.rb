f342c6787849729f884c4773d8917843467bc699
Murage-Ivy <ivymurage2000@gmail.com>
2023-04-14 13:49:57 +0300

    Create a custom route for sign up

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :posts
   resources :comments
   resources :users
+
+  post "/signup", to: "users#create"
+  # post "/login", to: "users#login"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
