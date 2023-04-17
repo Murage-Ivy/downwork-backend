6068e8d4f06c72b9125ab7699dcdeef5e1378371
Murage-Ivy <ivymurage2000@gmail.com>
2023-04-17 20:17:20 +0300

    Current user can comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,7 +17,7 @@ class CommentsController < ApplicationController
   end
 
   def create
-    comment = Comment.create!(comment_params)
+    comment =current_user.comments.create!(comment_params)
     render json: comment, status: :created
   end
 
